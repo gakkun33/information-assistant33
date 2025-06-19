e411281f8f73e53426635d8ddb868198bb491680
gakkun33 <gakkun36@icloud.com>
2025-06-19 11:41:32 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB1kZl3nvohEa35-lv6LPIm2brWRxTn4f0",
+  authDomain: "infomation-assistant33.firebaseapp.com",
+  projectId: "infomation-assistant33",
+  storageBucket: "infomation-assistant33.firebasestorage.app",
+  messagingSenderId: "993568139699",
+  appId: "1:993568139699:web:23dfba10652490661f03c6"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -33,11 +33,3 @@ export default function App() {
     <div className="p-4 text-xl">Link Manager App Ready. Please configure Firebase and add logic.</div>
   );
 }
-const firebaseConfig = {
-  apiKey: "AIzaSyB1kZl3nvohEa35-lv6LPIm2brWRxTn4f0",
-  authDomain: "infomation-assistant33.firebaseapp.com",
-  projectId: "infomation-assistant33",
-  storageBucket: "infomation-assistant33.firebasestorage.app",
-  messagingSenderId: "993568139699",
-  appId: "1:993568139699:web:23dfba10652490661f03c6"
-};
